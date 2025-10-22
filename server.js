cbc4cfa7e36b10ee76ea812b86ca4ce37a805274
Golam Morshed <golamdotmorshed@gmail.com>
2025-10-22 16:30:02 +0600

    Initial setup

---
+++ server.js
@@ -0,0 +1,12 @@
+import jsonServer from 'json-server'
+
+const server = jsonServer.create()
+
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+ 
+server.use(middlewares)
+server.use('/api', router)
+server.listen(process.env.PORT || 5000, () => {
+  console.log('JSON Server is running')
+})
